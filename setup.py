dc41c91cfbdd560f2b20ba1623427b7c83024d8d
bernaA96 <bernaabdulaziz@gmail.com>
2024-11-10 14:33:46 +0100

    make repository pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    description='A simple math quiz game',
+    url='https://github.com/bernaA96/dsss_homework_2_BernaA',
+    author='Berna Abdulaziz',
+    author_email='berna.abdulaziz@fau.de',
+    license='Apache-2.0 license',
+    packages=['math_quiz'],
+)
